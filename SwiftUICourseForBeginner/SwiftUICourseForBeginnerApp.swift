9089d8458124d47206a8f95a24a3891c9de82636
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-12-30 13:22:53 -0800

    Initial Commit

---
+++ SwiftUICourseForBeginner/SwiftUICourseForBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICourseForBeginnerApp.swift
+//  SwiftUICourseForBeginner
+//
+//  Created by Максим Минаков on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICourseForBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
