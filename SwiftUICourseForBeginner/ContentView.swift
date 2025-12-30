e186bf53a983ccbb13e12c9c13f82f7937f2cba6
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-12-30 14:40:23 -0800

    lesson_1

--- SwiftUICourseForBeginner/ContentView.swift
+++ SwiftUICourseForBeginner/ContentView.swift
@@ -3,10 +3,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Image("avatar_1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .clipShape(.circle)
+                    .overlay(Circle()
+                        .stroke(Color.blue, lineWidth: 4))
+                    .shadow(radius: 30)
+                
+                Text("Maksim Minacov")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text("IOS developer")
+                    .font(.callout)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.indigo)
+            }
+            Divider()
+            
+            
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    Image(systemName: "phone.fill")
+                    Text("+(7)9005918911")
+                }
+                
+                HStack {
+                    Image(systemName: "envelope.fill")
+                    Text("maksim.minakov.83@mail.ru")
+                }
+            }
+            .font(.headline)
+            .padding()
+            
+            Spacer()
         }
         .padding()
     }
@@ -15,7 +48,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-
-
-
